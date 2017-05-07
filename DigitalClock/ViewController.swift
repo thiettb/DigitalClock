54597ed270c5f4234ca4511f2826b627c7582c9a
Macbook <macbook@MacBook-Pro-cua-Macbook.local>
2017-05-07 19:45:24 +0700

    First Commit

--- DigitalClock/ViewController.swift
+++ DigitalClock/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeInDayLabel: UILabel!
+    @IBOutlet weak var grubLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        oclock()
+        _ = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(ViewController.oclock), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func oclock(){
+        let date = Date()
+        let calendar = Calendar.current
+        
+        var hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        
+        if hour > 12{
+            hour = hour - 12
+            grubLabel.text = "PM"
+        }else{
+            grubLabel.text = "AM"
+        }
+        
+        timeInDayLabel.text = hour.description + ":" + minute.description
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,3 @@
+Digital O'clock written by Tran Ba Thiet for iOS 10 
+Language: Swift 3.0
+MIT license
